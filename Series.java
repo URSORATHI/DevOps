34ecabf59d52260e25a6978eb4499bd991f88bc7
URSORATHI <56110107+URSORATHI@users.noreply.github.com>
2021-11-01 13:54:43 +0530

    Create Series.java

---
+++ Series.java
@@ -0,0 +1,22 @@
+import static java.lang.Math.pow;
+ 
+class Series
+{
+
+static double sum( int n)
+{
+    double i, total = 0;
+    for (i = 1; i <= n; i++)
+    {
+        total = total + 1/(Math.pow(i, i));
+                
+    }
+    return total;
+}
+ 
+public static void main(String args[])
+{
+    int n = Integer.parseInt(args[0]);
+    System.out.printf("%.2f", sum(n));
+}
+}
